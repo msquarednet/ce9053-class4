229ccccba115092689f7fb8d8a0cc9552b5df588
Mark Michaels <msquarednet@gmail.com>
2015-02-26 01:03:47 +0000

    init commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var fizzbuzz = required("ek_fb");
+var foo = require("foo");
+var fs = require("fs");
+
+console.log(fs.readFile);
+fs.readFile("README.md", function(err, data) {
+    console.log(data.toString());
+})
+
+
+console.log(fizzbuzz);
+console.log(foo);
